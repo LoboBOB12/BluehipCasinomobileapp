0b1c502484a77f33a63389dcf97ab79feda718f3
LoboBOB12 <63793995+LoboBOB12@users.noreply.github.com>
2023-05-30 00:18:51 +0300

    Initial commit

---
+++ app/src/main/java/app/lobo/bluehipcasinomobileapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package app.lobo.bluehipcasinomobileapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val imageView1 = findViewById<ImageView>(R.id.imageView)
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+
+        imageView1.setOnClickListener {
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+        imageView2.setOnClickListener {
+            val intent = Intent(this, policy::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
